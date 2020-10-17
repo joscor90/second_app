22e44de3a57da59eb5d37addbf53ae8be5511cb5
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-17 16:21:45 -0300

    Fixed header background image in main.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery.min.js
+//= require jquery.dropotron.min.js
+//= require jquery.scrollex.min.js
+//= require browser.min.js
+//= require breakpoints.min.js
+//= require util.js
+//= require main.js
